e30d3bb892f2ed7ece589acf2a9751d90fcc3d4a
MGaliana <lodoss07@gmail.com>
2017-01-11 18:01:55 +0100

    modulos vs paquetes

---
+++ Fase 4 - Temas avanzados/Tema 11 - Módulos/Leccion 01 - Modulos/test/test.py
@@ -0,0 +1,8 @@
+#from saludos import Saludo
+import sys
+
+sys.path.insert(1, "..")
+#print(sys.path)
+
+from saludos import Saludo
+Saludo()
---
+++ Fase 4 - Temas avanzados/Tema 11 - Módulos/Leccion 02 - Paquetes/paquete/adios/despedidas.py
@@ -0,0 +1,9 @@
+# Este es un modulo  con funciones de despiden
+
+def despedirse():
+	print("Adios, me estoy despidiendo desde la funciona de la modulo despedidas")
+
+
+class Despedida():
+	def __init__(self):
+		print("Adios, me estoy despidiendo desde la init de la clase despedidas")
