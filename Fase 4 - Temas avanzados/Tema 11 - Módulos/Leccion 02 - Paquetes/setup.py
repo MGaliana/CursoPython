860be94a090018c08f0bbddccb07477608898643
MGaliana <lodoss07@gmail.com>
2017-01-13 14:18:21 +0100

    Modulos

---
+++ Fase 4 - Temas avanzados/Tema 11 - Módulos/Leccion 02 - Paquetes/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+
+	name = "paquete",
+	version = "0.1",
+	description = "Este es un paquete de ejemplo",
+	author = "Miguel Galiana",
+	author_email = "yo@gmail.com",
+	url = "http://yo.es",
+	scripts = [],
+	packages = ["paquete","paquete.adios","paquete.hola"]
+	
+)
+
