8da9fb1b6ebd7e4a8efbe5bc0f867697f331bc0b
MGaliana <lodoss07@gmail.com>
2017-01-16 17:33:17 +0100

    Manejo de ficheros

---
+++ Fase 4 - Temas avanzados/Tema 11 - Módulos/Leccion 02 - Paquetes/paquete/saludos.py
@@ -0,0 +1,9 @@
+# Este es un modulo  con funciones de Saludar
+
+def saludar():
+	print("hola, te estoy saludando desde una funcion de saludos")
+
+
+class Saludo():
+	def __init__(self):
+		print("Hola Estoy saludando desde una clase")
